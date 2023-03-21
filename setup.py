5561cce902dad4bc0f824abbc4ee41d3841b08d0
C-Hurtmann <constantine2903@gmail.com>
2023-03-21 19:37:04 +0200

    attempt 5

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.9',
+    version='0.1.10',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(include=['bot.*', 'bot']),
     install_requires=['inquirer', 'colorama'],
     entry_points={'console_scripts': ['helper = bot.interface:main']}
 )

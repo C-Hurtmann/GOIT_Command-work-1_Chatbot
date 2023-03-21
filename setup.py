5d51bce297d56dbe850b54cd8f46fdb49227bcea
C-Hurtmann <constantine2903@gmail.com>
2023-03-21 19:48:29 +0200

    attempt6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.11',
+    version='0.1.12',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',

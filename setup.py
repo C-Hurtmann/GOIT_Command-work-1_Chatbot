0230104a71db48f5a8c12af84db5b75f15017407
C-Hurtmann <constantine2903@gmail.com>
2023-03-21 08:38:38 +0200

    removed tree

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.4',
+    version='0.1.5',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',
     packages=find_namespace_packages('bot'),
     install_requires=['inquirer', 'colorama'],
-    entry_points={'console_scripts': ['helper = bot.interface:main']}
+    entry_points={'console_scripts': ['helper = interface:main']}
 )
+

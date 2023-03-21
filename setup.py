eae3a7ea95b4efa8229aa330032edf3fd07e2aab
C-Hurtmann <constantine2903@gmail.com>
2023-03-21 08:02:36 +0200

    delete entry points

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.2',
+    version='0.1.3',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',
     packages=find_namespace_packages(),
     install_requires=['inquirer', 'colorama'],
-    entry_points={'console_scripts': ['helper = bot.interface:main']}
+    #entry_points={'console_scripts': ['helper = bot.interface:main']}
 )

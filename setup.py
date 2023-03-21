a3db7fb005f46d0e7de4159db83d831d9d3ba148
C-Hurtmann <constantine2903@gmail.com>
2023-03-21 07:47:20 +0200

    added successfuly deloying setup (doesn't work)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.0',
+    version='0.1.2',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',

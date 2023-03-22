6795995785eeee6edbf35ed9c2189b9d3e56155a
C-Hurtmann <constantine2903@gmail.com>
2023-03-22 08:59:38 +0200

    added prettytable to reqs

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GTTD',
-    version='0.1.14',
+    version='0.1.15',
     description='Console bot organizer',
     url='https://github.com/C-Hurtmann/Going_to_the_Dream',
     license='MIT',
     packages=find_namespace_packages(include=['bot.*', 'bot']),
-    install_requires=['inquirer', 'colorama'],
+    install_requires=['inquirer', 'colorama', 'prettytable'],
     entry_points={'console_scripts': ['helper = bot.interface:main']}
 )
